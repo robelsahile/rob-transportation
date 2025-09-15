5bf24ac5f5af81756529e92408a4f156eec8fabf
Robel Sahile <robelsahile@Robels-MacBook-Pro.local>
2025-09-15 14:59:27 -0700

    update

--- src/components/PaymentSuccess.tsx
+++ src/components/PaymentSuccess.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { BookingFormData } from "../types";
 
 type PricingSnapshot = {
